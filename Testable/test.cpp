53776a4424b780cbc0d7054ca9ecf1c668300102
minq <james2224@hanmail.net>
2024-07-03 10:55:13 +0900

    [feature] Page33

--- Testable/test.cpp
+++ Testable/test.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
+#include "../PrimeFactorProject/PrimeFactor.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <vector>
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+
+    PrimeFactor pf;
+    vector<int> expected = {};
+    EXPECT_EQ(expected, pf.of(1));
 }
