593908f6fd7093a737df3722ded231174dca81ca
minq <james2224@hanmail.net>
2024-07-03 11:08:27 +0900

    [refactoring] Page52

--- PrimeFactorProject/PrimeFactor.cpp
+++ PrimeFactorProject/PrimeFactor.cpp
@@ -9,6 +9,7 @@ public:
         vector<int> result = {};
         if (number > 1)
         {
+            int divisor = 2;
             if (number == 4)
             {
                 while (number % 2 == 0)
@@ -19,8 +20,14 @@ public:
             }
             else if (number == 6)
             {
-                result.push_back(2);
-                result.push_back(3);
+                for (divisor = 2; number > 1; divisor++)
+                {
+                    while (number % divisor == 0)
+                    {
+                        result.push_back(divisor);
+                        number /= divisor;
+                    }
+                }
             }
             else
             {
