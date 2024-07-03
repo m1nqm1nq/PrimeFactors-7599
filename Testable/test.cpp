74a0378e051f75fa34670b5131ef54f5a191ae60
minq <james2224@hanmail.net>
2024-07-03 11:01:40 +0900

    [refactoring] Page38

--- Testable/test.cpp
+++ Testable/test.cpp
@@ -4,16 +4,21 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
-
+class PrimeFactorFixture : public testing::Test
+{
+public:
     PrimeFactor pf;
-    vector<int> expected = {};
+    vector<int> expected;
+};
+
+TEST_F(PrimeFactorFixture, Of1)
+{
+    expected = {};
     EXPECT_EQ(expected, pf.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-
-    PrimeFactor pf;
-    vector<int> expected = {2};
+TEST_F(PrimeFactorFixture, Of2)
+{
+    expected = {2};
     EXPECT_EQ(expected, pf.of(2));
 }
